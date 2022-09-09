127ba0dbfef96823427a3a3e09f4eef376b15d74
dliscano <dliscano@uninorte.edu.co>
2022-09-08 21:14:43 -0500

    mensaje

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -5,6 +5,7 @@ public class GeneradorVertebrados {
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
         miVertebrados[2] = new Vertebrado("Tiburón", "Pez");
         miVertebrados[3] = new Vertebrado("Gato", "Mamifero");
+        miVertebrados[4] = new Vertebrado("Leon", "Mamifero");
         
         return miVertebrados;
     }
